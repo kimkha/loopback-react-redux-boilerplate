49312003b23e29ea8ac9e411d721e4b29b1dd4bd
Nguyễn Kim Kha <nkimkha@gmail.com>
2017-09-11 22:48:28 +0800

    Add layout

--- client/Layout.js
+++ client/Layout.js
@@ -2,25 +2,52 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose } from 'recompose';
-import { AppBar, Toolbar, Typography } from 'material-ui';
+import { convertAuthenState } from 'restful-api-redux';
+import { AppBar, Toolbar, Typography, Button } from 'material-ui';
 
 class Layout extends Component {
+
+  handleLogin = () => {
+    console.log('dddd');
+    this.props.push('/login');
+  };
+
   render() {
     const { children } = this.props;
 
     return (
       <div>
         <AppBar position="static" color="primary">
-          <Toolbar>
-            <Typography type="title" color="inherit">
-              Title
-            </Typography>
-          </Toolbar>
+          {this.renderToolBar()}
         </AppBar>
         <div style={{ padding: 20 }}>
           {children}
         </div>
       </div>
+    );
+  }
+
+  renderToolBar() {
+    const { status } = this.props;
+    return (
+      <Toolbar>
+        <Typography type="title" color="inherit">
+          Title
+        </Typography>
+        {status === 'AUTHENTICATED' ? this.renderUserTopBox() : this.renderLogin()}
+      </Toolbar>
+    );
+  }
+
+  renderUserTopBox() {
+    return (
+      <Button color="contrast" onClick={this.handleLogin}>User</Button>
+    )
+  }
+
+  renderLogin() {
+    return (
+      <Button color="contrast" onClick={this.handleLogin}>Login</Button>
     );
   }
 }
@@ -30,7 +57,10 @@ Layout.propTypes = {};
 Layout.defaultProps = {};
 
 function mapStateToProps(state, props) {
-  return {};
+  const authen = convertAuthenState(state);
+  return {
+    status: authen.status,
+  };
 }
 
 const enhance = compose(
